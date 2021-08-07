87f5ee7c2e445dbb9cf62dabf0b43eaf316d040d
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-07 17:54:04 -0400

    Update Main.php

--- src/LunarMoon72/FormCode1/Main.php
+++ src/LunarMoon72/FormCode1/Main.php
@@ -3,56 +3,48 @@
 namespace LunarMoon72\FormCode1;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\plugin\FormAPI;
 
 use pocketmine\Player;
 use pocketmine\Server;
+
+use pocketmine\item\Item;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 use pocketmine\event\Listener;
 
+use musqit\invmenu\InvMenu;
+use musqit\invmenu\InvMenuHandler;
+
+use pocketmine\inventory\transaction\action\SlotChangeAction;
 
 class Main extends PluginBase implements Listener{
-	public function onEnabled(){
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		$this->getLogger()->info("Plugin is enabled");
-	}
-	
-	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
-        switch($cmd->getName()){
-            case "form1":
-              if($sender instanceof Player){
-                  $this->form($sender);
-              } else {
-                     $sender->sendMessage("You arnt a player, to open the form; join the game.");
-              }
-        }
-        return true;
-    } 
-    public function form($player){
-    	$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-    	$form = $api->createSimpleForm(function (Player $player, int $data = null){
-    		$result = $data;
-    		if($result === null){
-    			return true;
-    		}
-    		switch($result){
-    			case 0:
-                  $player->sendMessage("Search up LunarMoon72 on youtube!");
-    			break;
-
-    			case 1:
-                  $player->sendMessage("Go join the discord now!");
-    			break;
-    		}
-
-    	});
-    	$form->setTitle("Form Example");
-    	$form->setContent("Press a button!");
-    	$form->addButton("Youtube");
-    	$form->addButton("Discord");
+    public function onEnabled(){
+     if(!InvMenuHandler::isRegistered()){
+      InvMenuHandler::register($this);
     }
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        $this->getLogger()->info("Plugin is enabled");
+
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+    switch($cmd->getName("inv")){
+      case "inv":
+        if($sender instanceof Player){
+          $menu = InvMenu::create(InvMenu::TYPE_CHEST);
+          $inventory = $menu->getInventory()->setContents([
+             Item::get(Item::DIAMOND_SWORD),
+             Item::get(Item::DIAMOND_PICKAXE)
+          ]);
+          $menu->setName("CustomCrate")
+          $menu->getInventory()->addItem(Item::get(Item::DIAMOND_AXE));
+          $menu->getInventory()->setItem(3, Item::get(Utem::GOLD_INGOT));
+          $menu->send($player);
+        }
+
+    }
+  }
+
 
 }
